73f2cdbb507baccfb40d5a8bbcca7cd1faa0e917
Towkin <emanuel.stromgren@pulpeten.se>
2020-01-31 19:18:53 +0100

    Added super-simple and broken Player Controller and prefab, as well as a ship prefab.

---
+++ Adrift/Assets/Master/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Adrift.Game
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private float 
+            m_MoveSpeed = 10f,
+            m_MaxSpeed = 5.0f;
+
+        
+
+
+        [SerializeField]
+        private Camera m_PlayerCamera = null;
+
+        public Rigidbody PlayerBody { get; private set; }
+        private void Awake()
+        {
+            PlayerBody = GetComponent<Rigidbody>();
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var accelerationVector = m_PlayerCamera.transform.rotation * new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+            PlayerBody.AddForce(accelerationVector * m_MoveSpeed, ForceMode.Acceleration);
+            if (PlayerBody.velocity.sqrMagnitude > m_MaxSpeed * m_MaxSpeed)
+                PlayerBody.velocity = PlayerBody.velocity.normalized * m_MaxSpeed;
+
+            m_PlayerCamera.transform.rotation = Quaternion.Euler(m_PlayerCamera.transform.eulerAngles + new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0f));
+        }
+    }
+}
