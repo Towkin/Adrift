07cfc9e34c499138367b34244c99df771797a323
Emanuel Strömgren <emanuel.stromgren@pulpeten.se>
2020-02-02 11:40:17 +0100

    Added Audio zone script and prefab.

---
+++ Adrift/Assets/Master/Interaction/AudioZone.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Adrift.Game
+{
+    public class AudioZone : MonoBehaviour, IAutoInteractable
+    {
+        [SerializeField]
+        private FMODUnity.StudioEventEmitter m_Audio = null;
+
+        [SerializeField]
+        private string m_Parameter = "";
+
+        public bool Enter(GameObject actor)
+        {
+            if (m_Audio != null && m_Audio.EventInstance.hasHandle())
+                m_Audio.SetParameter(m_Parameter, 1f);
+            return true;
+        }
+
+        public bool Exit(GameObject actor)
+        {
+            if (m_Audio != null && m_Audio.EventInstance.hasHandle())
+                m_Audio.SetParameter(m_Parameter, 0f);
+            return true;
+        }
+    }
+}
